edc66a3b34d3f06e0ff6ea504602295a571da1ab
Hunton <edh04@ostml0254144.home>
2018-01-18 16:33:09 +0000

    more

--- server/main.js
+++ server/main.js
@@ -102,12 +102,12 @@ const watchFolders = function(){
 
   fs.watch('./server/fragments/original', { recursive: false }, function(evt, name) {
     origCount.push(name);
-    console.log("OG COUNT: "+origCount)
-    console.log("LEN "+origCount.length)
+  ///  console.log("OG COUNT: "+origCount)
+  //  console.log("LEN "+origCount.length)
     if(origCount.length > 20){
 
       fs.unlink('./server/fragments/original/'+origCount[0], function(){
-        console.log("unlink");
+      //  console.log("unlink");
              origCount.shift();
       });
 
@@ -160,11 +160,7 @@ const cInterval = function(intervalName, type){
             downloadChunk(timecodes[0], 'hostA');
             downloadChunk(timecodes[0], 'hostB');
             downloadChunk(timecodes[0], 'hostC');
-<<<<<<< HEAD
           //  downloadChunk(timecodes[0], 'hostD');
-=======
-            downloadChunk(timecodes[0], 'hostD');
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
 
           }
@@ -200,25 +196,15 @@ const downloadChunk = function(time, interval){
 
   var url = 'http://'+host+'/z2skysportsmainevent/1301.isml/QualityLevels(4864960)/Fragments(video='+time+')';
 
-<<<<<<< HEAD
   //console.log("The URL: "+url);
 
   let options = {
-=======
-  console.log("The URL: "+url);
-
-  const options = {
->>>>>>> b785978e346fed56f737afef977d657b8729b558
     url: url,
     method: 'GET',
     headers: {
         'Accept': 'application/json',
         'Accept-Charset': 'utf-8',
-<<<<<<< HEAD
         'User-Agent': 'fragment-puller'
-=======
-        'User-Agent': 'my-reddit-client',
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
     }
 
@@ -226,14 +212,11 @@ const downloadChunk = function(time, interval){
 
 };
 
-<<<<<<< HEAD
 if(interval !== 'original'){
 
   options.headers.Host =  'origin7.skysportsmainevent.hss.skydvn.com';
 }
 
-=======
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
 var filename = './server/fragments/'+interval+'/chunk_'+time+'.mp4';
 
@@ -245,11 +228,7 @@ request(options, function(err, res, body){
 
   if(!filesToTest[time]){
 
-<<<<<<< HEAD
        filesToTest[time] = {'original':'', 'hostA':'', 'hostB':'', 'hostC':'', 'counter':0}
-=======
-       filesToTest[time] = {'original':'', 'hostA':'', 'hostB':'', 'hostC':'', 'hostD':'','counter':0}
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
   }
 
@@ -281,7 +260,6 @@ const equivalence = function(obj, sizes){
   function allEqual(arr) {
     for(var i = 0; i <arr.length-1; i++ ){
 
-<<<<<<< HEAD
           if(arr[i] != arr[i+1]){
             return false;
           }
@@ -291,56 +269,7 @@ const equivalence = function(obj, sizes){
 
   var EQ = allEqual(sizes);
 
-=======
-
-
-    for(var i = 0; i <arr.length-1; i++ ){
-
-      console.log("Val of i "+arr[i]);
-      console.log("Val of i+1 "+arr[i+1]);
-
-          if(arr[i] != arr[i+1]){
-            return false
-          }
-
-    }
-
-    return true;
-  }
-
-
-  var EQ = allEqual(sizes);
-
-
-
->>>>>>> b785978e346fed56f737afef977d657b8729b558
   if(EQ === true) {
-    /*fs.unlink(obj.original, function(err){
-        console.log("del Orig");
-        fs.unlink(obj.hostA, function(err){
-            console.log("del A");
-
-          fs.unlink(obj.hostB, function(err){
-              console.log("del B");
-            fs.unlink(obj.hostC, function(err){
-                console.log("del C");
-
-<<<<<<< HEAD
-            })
-=======
-    console.log("All the same");
-    fs.unlink(obj.original);
-    fs.unlink(obj.hostA);
-    fs.unlink(obj.hostB);
-    fs.unlink(obj.hostC);
-    fs.unlink(obj.hostD);
->>>>>>> b785978e346fed56f737afef977d657b8729b558
-
-          })
-
-        })
-
-      })*/
 
     } else {
       moveThem(obj)
@@ -352,7 +281,7 @@ const moveThem = function(obj){
   delete obj['counter'];
 
   console.log("MOVE SOME FILES SOMWHERE");
-  console.log("THE OBJ "+util.inspect(obj, false, null));
+  //console.log("THE OBJ "+util.inspect(obj, false, null));
 
   for(var key in obj){
 
@@ -360,20 +289,15 @@ const moveThem = function(obj){
 
           var s = obj[key]
 
-          console.log("The value of S: "+s);
+          //console.log("The value of S: "+s);
 
-<<<<<<< HEAD
             var bits = s.split('/');
             var host = bits[3]
             var fileName = bits[4];
-=======
-      let statsO = fs.statSync(obj.original);
-      sizes.origin_fileSizeInBytes = statsO.size;
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
             fs.rename(obj[key], './server/fragments/non-equals/'+host+'_'+fileName, function(){
 
-              console.log("MOVE FILE");
+            //  console.log("MOVE FILE");
 
             });
 
@@ -381,10 +305,6 @@ const moveThem = function(obj){
   }
 
 
-<<<<<<< HEAD
-=======
-      equivalence(obj, sizes);
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
   }
 
@@ -420,32 +340,11 @@ const testThem = function(obj){
 
 const downloadManifest = function(intName){
   request.get('http://skysportsmainevent-go-hss.ak-cdn.skydvn.com/z2skysportsmainevent/1301.isml/Manifest', function(err,res,body) {
-<<<<<<< HEAD
     parseString(body, function (err, result) {
           let currentTimeCode = result.SmoothStreamingMedia.StreamIndex[0].c[0].$.t;
             timecodes.push(parseInt(currentTimeCode));
                  downloadChunk(currentTimeCode, 'original');
             });
-=======
-
-        console.log("first request");
-
-        parseString(body, function (err, result) {
-
-          //  console.log("string parsed: "+result);
-
-            let currentTimeCode = result.SmoothStreamingMedia.StreamIndex[0].c[0].$.t;
-
-            //var offSet = 20000000*53;
-
-            //timecodes.push(currentTimeCode+offSet);
-            timecodes.push(parseInt(currentTimeCode));
-
-
-            downloadChunk(currentTimeCode, 'original');
-
-        });
->>>>>>> b785978e346fed56f737afef977d657b8729b558
 
      });
    }
