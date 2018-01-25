c19d57dfd017081ed4f8c815b6c8b71c82e209b3
Edward Hunton <edward.hunton@sky.uk>
2018-01-25 11:22:04 +0000

    logging and arrgs added

--- server/main.js
+++ server/main.js
@@ -16,10 +16,48 @@ import rimraf from 'rimraf';
 
 import dateFormat from 'dateFormat';
 
+const MANIFEST_WARNING = "THERE WAS AN ISSUE DOWNLOADING THE MANIFEST";
+const FRAGMENT_WARNING = "THERE WAS AN ERROR REQUESTING THE FRAGMENT";
+const EQUALITY_MESSAGE = "FRAGMENTS TESTED EQUAL IN SIZE";
+const NONEQUALITY_MESSAGE = "FRAGMENTS TESTED NON-EQUAL IN SIZE";
+const ERROR_EQUALITY_MESSAGE = "FRAGMENTS TESTED NON-EQUAL IN SIZE";
+
+let filesToTest = {};
+let timecodes = []; // rea from the hss manifests
+let intervalA, intervalB;
+
+const bitRates = ["89984","280000", "619968", "1179968", "2014976", "3184960", "4864960"];
+
+const hosts = {
+  'original':{
+    'ip': 'skysportsmainevent-go-hss.ak-cdn.skydvn.com'
+  },'hostA':{
+    'ip': '90.211.176.20'
+  },'hostB':{
+    'ip': '90.211.176.148'
+  },'hostC':{
+    'ip': '2.122.212.14'
+  },'hostD':{
+    'ip': '2.122.212.142'
+  }
+}
+
+
+
+const Q_index = process.argv[4] || 6;;
+
+const fragpath = './fragments/';
+
+const mainIntervalLength = 60000;
+
+const fragmentOffSet = process.argv[3] || 53; // from oldest chunk to live
+
+let streamString = process.argv[2] || 'skysportsmainevent-go-hss.ak-cdn.skydvn.com/z2skysportsmainevent/1301';
+
+
+
 
 const streamParse = function(){
-
-  let streamString = process.argv[2] || 'skysportsmainevent-go-hss.ak-cdn.skydvn.com/z2skysportsmainevent/1301';
 
   let streamObj = {};
 
@@ -39,8 +77,6 @@ const streamParse = function(){
 }
 
 
-const MANIFEST_WARNING = "THERE WAS AN ISSUE DOWNLOADING THE MANIFEST";
-const FRAGMENT_WARNING = "THERE WAS AN ERROR REQUESTING THE FRAGMENT";
 
 
 
@@ -54,44 +90,7 @@ const streamObj = streamParse();
 });*/
 
 
-let timecodes = []; // rea from the hss manifests
 
-var bitRates = ["89984","280000", "619968", "1179968", "2014976", "3184960", "4864960"];
-
-var hosts = {
-  'original':{
-    'ip': 'skysportsmainevent-go-hss.ak-cdn.skydvn.com'
-  },'hostA':{
-    'ip': '90.211.176.20'
-  },'hostB':{
-    'ip': '90.211.176.148'
-  },'hostC':{
-    'ip': '2.122.212.14'
-  },'hostD':{
-    'ip': '2.122.212.142'
-  }
-}
-
-var Q_index = 6;
-
-var filesToTest = {};
-
-var fragpath = './fragments/';
-
-var mainIntervalLength = 60000;
-
-
-
-//var folderNames = ['hostA','hostB','hostC','hostD', 'original','non-equals'];
-
-//var chunkFolders = ['hostA','hostB','hostC','hostD'];
-
-var chunkOffSet = 53; // from oldest chunk to live
-
-let intervalA, intervalB;
-
-const debug = require('debug')('my-namespace')
-const name = 'my-app'
 
 const deleteFolder = function(path, callback){
   rimraf(path, function(){
@@ -183,7 +182,7 @@ const createTimeoutForIntervalB = function(){
 
 const createLogFile = function(){
   //console.log(__dirname+'/fragments/logs/logFile.txt');
-  fs.writeFile('./logs/logFile.txt', 'TEST STARTED: '+new Date()+'\n', (err) => {
+  fs.writeFile('./logs/logFile.txt', 'TEST STARTED: '+new Date()+', STREAM UNDER TEST: '+streamString+' , BITRATE: '+bitRates[Q_index]+', FRAGMENT OFFSET: '+fragmentOffSet+'\n', (err) => {
     if (err) {
       throw err;
     }
@@ -217,6 +216,8 @@ const chunkCheckInterval = function(){
 
 
 const getOptions = function(streamObj, interval, url){
+
+  console.log("URL"+url);
 
   let options = {
       time: true,
@@ -258,22 +259,18 @@ const performRequest = function(options, time, interval, filename, callback, _ho
 
   request(options, function(err, res, body){
 
-    console.log("The err  "+err);
 
-    //if(err){
 
-    //  console.log(FRAGMENT_WARNING);
-    //  log('FRAGMENT DOWNLOAD ERROR: '+options.url);
+    if(err !== null || res.statusCode !== 200){
+      console.log(FRAGMENT_WARNING);
+      log(FRAGMENT_WARNING+options.url);
+      clearInterval(intervalB);
+      return;
 
-    //  clearInterval(intervalB);
-    //  return;
-
-  //  } else {
+    } else {
 
       if(!filesToTest[time]){
             filesToTest[time] = _hosts;
-            //console.log("FTOT "+util.inspect(filesToTest, false, null));
-
             filesToTest[time].counter = 0;
           }
           filesToTest[time][interval].chunkPath = filename;
@@ -283,10 +280,8 @@ const performRequest = function(options, time, interval, filename, callback, _ho
                   filesToTest[time].counter++;
                 }
             }
-      //  }
+        }
     }).pipe(fs.createWriteStream(filename)).on('close', function(){
-      //console.log(filesToTest[time].counter);
-      //console.log(Object.keys(hosts).length);
       if(filesToTest[time]){
         if(filesToTest[time].counter === Object.keys(hosts).length-2 && filesToTest[time].original){
             callback(filesToTest[time]);
@@ -305,16 +300,11 @@ const performRequest = function(options, time, interval, filename, callback, _ho
 }
 
 const downloadChunk = function(time, interval){
+      let url = buildChunkUrl(streamObj, bitRates[Q_index], time);
+      let options = getOptions(streamObj, interval, url);
+      let fileName = buildFileName(fragpath, interval, bitRates[Q_index], time);
 
-  console.log("interval "+interval);
-
-
-let host = hosts[interval].ip;
-let url = buildChunkUrl(streamObj, bitRates[Q_index], time);
-let options = getOptions(interval, url);
-let fileName = buildFileName(fragpath, interval, bitRates[Q_index], time);
-
-performRequest(options, time, interval, fileName, testThem, hosts);
+      performRequest(options, time, interval, fileName, testThem, hosts);
 
 }
 
@@ -334,13 +324,13 @@ const equivalence = function(obj, sizes){
 
   if(EQ === true) {
 
-    console.log("THEY ARE ALL EQUAL");
+    console.log(EQUALITY_MESSAGE);
 
     return true;
 
   } else {
 
-    console.log("THEY ARE NOT ALL EQUAL");
+    console.log(NONEQUALITY_MESSAGE);
 
     return false;
 
@@ -354,45 +344,15 @@ const moveThem = function(obj){
   delete obj['counter'];
   delete obj['totaltime'];
 
-//  console.log("MOVE SOME FILES SOMWHERE");
-  //g
-
   for(var key in obj){
-
-        if(obj.hasOwnProperty(key) && obj[key] !== ''  ){
-
-          var s = obj[key]
-
-        //console.log("The value of S: "+s);
-
+    if(obj.hasOwnProperty(key) && obj[key] !== ''  ){
+          var s = obj[key];
           var bits = s.split('/');
           var host = bits[7];
           var fileName = bits[8];
-
-
-
-
-          fs.createReadStream(obj[key]).pipe(fs.createWriteStream('./server/fragments/non-equals/'+host+'_'+fileName));
-
-        }
+          fs.createReadStream(obj[key]).pipe(fs.createWriteStream('./fragments/non-equals/'+host+'_'+fileName));
+    }
   }
-
-}
-
-const whatQ = function(tt){
-  //console.log(tt);
-
-  if(tt < 1.75 || tt > 2.25){ // only change if sub optimal
-
-   if(tt < 1.75 && Q_index+1 < bitRates.length-1){
-     console.log("jump up");
-      Q_index++;
-   } else if(tt > 2.25 && Q_index-1 >= 0) {
-       console.log("jump down");
-       Q_index--;
-     }
-
-   }
 
 }
 
@@ -401,13 +361,7 @@ const testThem = function(obj){
 
 
   var now = new Date();
-
   var D = dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-
-  console.log("test them"+obj);
-
-  console.log(__dirname);
-  console.log(fragpath);
 
   let sizes = [];
 
@@ -421,11 +375,10 @@ const testThem = function(obj){
                         sizes.push(stats.size);
                 });
              } else {
-
-               //console.log("TEST ERROR");
-                var bits = obj.original.chunkPath.split('/');
-                log('TEST: '+D+' - '+bits[bits.length-1]+' - test ERRORED');
-                moveThem(obj);
+              console.log(ERROR_EQUALITY_MESSAGE);
+              var bits = obj.original.chunkPath.split('/');
+              log('TEST: '+D+' - '+bits[bits.length-1]+' - '+ERROR_EQUALITY_MESSAGE);
+              moveThem(obj);
 
 
 
@@ -438,16 +391,15 @@ const testThem = function(obj){
       var str = 'TEST: '+D+' - '+bits[bits.length-1];
 
       if(EQ === false){
-          str+=' - tested UNEQUAL';
+          str+=' - '+NONEQUALITY_MESSAGE;
           console.log(str);
           log(str);
           moveThem(obj);
       } else {
-          str+=' - tested EQUAL';
+          str+=' - '+EQUALITY_MESSAGE;
           console.log(str);
           log(str);
       }
-
     }
 
 
@@ -456,32 +408,28 @@ const downloadManifest = function(callback, streamObj){
 
    var url = buildManifestUUrl(streamObj);
 
-  console.log(url);
-
   request.get(url, function(err,res,body) {
+
+    //console.log("URL "+url);
+
     parseString(body, function (err, result) {
 
-      const errFunc = function(){
-        console.log(MANIFEST_WARNING);
-        log('MANIFEST DOWNLOAD ERROR: '+url);
-        clearInterval(intervalA);
-        return;
-      }
-
-      if(err){
-        errFunc();
-      }
-
-        if(result !== undefined){
+              const errFunc = function(){
+                console.log(MANIFEST_WARNING);
+                log(MANIFEST_WARNING+url);
+                clearInterval(intervalA);
+                return;
+              }
+              if(err !== null || res.statusCode !== 200){
+                errFunc();
+              } else {
 
                 let currentTimeCode = parseInt(result.SmoothStreamingMedia.StreamIndex[0].c[0].$.t);
-                let offSetChunk = currentTimeCode+(20000000*chunkOffSet);
+                let offSetChunk = currentTimeCode+(20000000*fragmentOffSet);
                 timecodes.push(parseInt(offSetChunk));
                 callback();
                 downloadChunk(parseInt(offSetChunk), 'original');
 
-              } else {
-                errFunc();
               }
           });
 
